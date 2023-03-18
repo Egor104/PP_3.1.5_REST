60768417959cb697829cb23b97ece8a068b8dbc6
Egor104 <egor.grig104@gmail.com>
2023-03-18 16:52:26 +0500

    q

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -2,10 +2,8 @@ package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.services.UserService;
 
 @Controller
@@ -15,19 +13,55 @@ public class AdminController {
     private final UserService userService;
     public AdminController(UserService userService) { this.userService = userService; }
 
-    @GetMapping("/admin")
-    public String userList(Model model) {
-        model.addAttribute("allUsers", userService.findAllUsers());
-        return "admin";
+    @GetMapping("/")
+    public String listUsers(Model model) {
+        model.addAttribute("users", userService.findAllUsers());
+        return "usersAll";
     }
 
-    @PostMapping("/admin")
-    public String  deleteUser(@RequestParam(required = true, defaultValue = "" ) Long userId,
-                              @RequestParam(required = true, defaultValue = "" ) String action,
-                              Model model) {
-        if (action.equals("delete")){
-            userService.deleteUserById(userId);
-        }
-        return "redirect:/admin";
+    @GetMapping("/userAdd")
+    public String showFormForAdd(Model model) {
+        model.addAttribute("user", new User());
+        return "userAdd";
     }
+
+    @PostMapping("/new_user")
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping( "/edit/{id}")
+    public String showFormForUpdate(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userService.findUserById(id));
+        return "userUpdate";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.updateUser(user);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteUserById(@PathVariable("id") Long id) {
+        userService.deleteUserById(id);
+        return "redirect:/";
+    }
+
+    //    @GetMapping("/admin")
+//    public String userList(Model model) {
+//        model.addAttribute("allUsers", userService.findAllUsers());
+//        return "admin";
+//    }
+//
+//    @PostMapping("/admin")
+//    public String  deleteUser(@RequestParam(required = true, defaultValue = "" ) Long userId,
+//                              @RequestParam(required = true, defaultValue = "" ) String action,
+//                              Model model) {
+//        if (action.equals("delete")){
+//            userService.deleteUserById(userId);
+//        }
+//        return "redirect:/admin";
+//    }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -18,44 +18,8 @@ public class UserController {
     }
 
     @GetMapping("/")
-    public String listUsers(Model model) {
-        model.addAttribute("users", userService.findAllUsers());
-        return "usersAll";
-    }
-
-    @GetMapping("/userAdd")
-    public String showFormForAdd(Model model) {
-        model.addAttribute("user", new User());
-        return "userAdd";
-    }
-
-    @PostMapping("/new_user")
-    public String saveUser(@ModelAttribute("user") User user) {
-        userService.saveUser(user);
-        return "redirect:/";
-    }
-
-    @GetMapping( "/edit/{id}")
-    public String showFormForUpdate(@PathVariable("id") Long id, Model model) {
+    public String showInfo(@PathVariable("id") Long id, Model model) {
         model.addAttribute("user", userService.findUserById(id));
-        return "userUpdate";
+        return "userInfo";
     }
-
-    @PostMapping("/edit/{id}")
-    public String updateUser(@ModelAttribute("user") User user) {
-        userService.updateUser(user);
-        return "redirect:/";
-    }
-
-    @PostMapping("/delete/{id}")
-    public String deleteUserById(@PathVariable("id") Long id) {
-        userService.deleteUserById(id);
-        return "redirect:/";
-    }
-
-//    @GetMapping("/userInfo")
-//    public String showInfo() {
-//
-//        return "userInfo";
-//    }
 }
