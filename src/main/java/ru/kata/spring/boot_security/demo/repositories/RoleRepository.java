c8e958700c0bdead9d192ea1b754b3e9435b308f
Egor104 <egor.grig104@gmail.com>
2023-03-18 22:07:35 +0500

    q

--- src/main/java/ru/kata/spring/boot_security/demo/repositories/RoleRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/RoleRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.model.Role;
 
-// Репозитории нужны для упрощённой реализации запросов к БД (не нужно прописывать SQL запросы)
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repositories/UserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.model.User;
 
-// Репозитории нужны для упрощённой реализации запросов к БД (не нужно прописывать SQL запросы)
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @Query("Select u from User u left join fetch u.roles where u.username=:username")
